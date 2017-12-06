8bf65c702ff26818961201150cf06e5366a7d575
Artur Wilanowski <arturwilanowski@gmail.com>
2017-12-06 22:56:32 +0100

    second commit

--- src/main/java/pl/wilanowskiartur/controllers/MainController.java
+++ src/main/java/pl/wilanowskiartur/controllers/MainController.java
@@ -1,4 +1,18 @@
 package pl.wilanowskiartur.controllers;
 
-public class MainController {
+import javafx.fxml.Initializable;
+import pl.wilanowskiartur.models.SocketConnector;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable{
+
+    private SocketConnector socketConnector = SocketConnector.getInstance();
+
+    public void initialize(URL location, ResourceBundle resources) {
+        socketConnector.connect();
+
+        socketConnector.sendMessage("Ping");
+    }
 }
--- src/main/java/pl/wilanowskiartur/models/SocketConnector.java
+++ src/main/java/pl/wilanowskiartur/models/SocketConnector.java
@@ -14,6 +14,7 @@ public class SocketConnector {
     }
 
     private WebSocketContainer container;
+    private Session session;
 
     private SocketConnector(){
         container = ContainerProvider.getWebSocketContainer();
@@ -33,7 +34,21 @@ public class SocketConnector {
 
     @OnOpen
     public void onOpen(Session session){
+        this.session = session;
         System.out.println("POLACZONO!");
+    }
+
+    public void sendMessage(String message){
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message){
+        System.out.println(message);
     }
 
 
